fe913e2f136bed3d0ff90cb6fba50c572341ecc5
Ben Lewis <ben@karmascience.com>
2013-02-03 00:45:38 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hopen::Application.config.secret_token = 'ec04663e897b4bd92a1b8c5cb4834fcf886d79b45f90afee799617a614c2fafdfee2a671167084420563ed0a1efaff23776217682b0e4a3f712926ffd717fdd4'
